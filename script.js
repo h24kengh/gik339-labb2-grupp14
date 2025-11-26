00cf2d33695c303c511a67c77b78e8eadf2c6fab
Kennedy Gharabi <h24kengh@du.se>
2025-11-26 17:41:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 /* Hämtar checkboxen med getElementById och lagrar den
 i variabeln checkbox */
-document.getElementById("checkbox")
 const checkbox = document.getElementById("checkbox")
 
 /* Hämtar alla textfält med getElementsByClassName och lagrar
 den i variabeln textfield */
-document.getElementsByClassName("textfield")
 const textfield = document.getElementsByClassName("textfield")
 
 /* Hämtar knappen med querySelector och lagrar den i
 variabeln button */
-document.querySelector('.button')
 const button = document.querySelector('button')
 
 /* Hämtar div-elementet med querySelectorAll och lagrar
 den i variabeln div */
-document.querySelectorAll('div')
 const div = document.querySelectorAll('div')
+
+
+function eventListener() {
+console.log(target)
+if  (id=="content")
+
+}
