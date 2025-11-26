d4ec229e91c4b74f78a2debcbed3ea86b2e5cc42
Kennedy Gharabi <h24kengh@du.se>
2025-11-26 13:39:21 +0100

    Hämta och lagra variabler

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-console.log("Labbgrupp 14")
+/* Hämtar checkboxen med getElementById och lagrar den
+i variabeln checkbox */
+document.getElementById("checkbox")
+const checkbox = document.getElementById("checkbox")
 
-console.log("hej")
+/* Hämtar alla textfält med getElementsByClassName och lagrar
+den i variabeln textfield */
+document.getElementsByClassName("textfield")
+const textfield = document.getElementsByClassName("textfield")
 
-console.log("hejdå")
+/* Hämtar knappen med querySelector och lagrar den i
+variabeln button */
+document.querySelector('.button')
+const button = document.querySelector('button')
+
+/* Hämtar div-elementet med querySelectorAll och lagrar
+den i variabeln div */
+document.querySelectorAll('div')
+const div = document.querySelectorAll('div')
